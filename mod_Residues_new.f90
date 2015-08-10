0315af181ac76fb5664d5cdd7c56e1ad900b06bf
rontsch <rontsch@fnal.gov>
2015-08-10 14:48:36 -0500

    added photon ID to tag_Z option in resid2

--- mod_Residues_new.f90
+++ mod_Residues_new.f90
@@ -1041,20 +1041,20 @@ END SUBROUTINE
 
 !          if ( (tag_f .eq. 1 .or. tag_f .eq. 2) .and. (Lab_ex(l2c(1)) .eq. 'zee') .and. (Lab_ex(l2c(2)-1) .eq. 'zee') ) then
 !         tag_Z=1 removes diagrams of a Z coupl. to a closed fermion loop coupl. to the rest 
+
           if ( (TreeProcs(1)%NumPart .eq. 3) .and. ( abs(TreeProcs(1)%PartType(1)) .eq. Bot_ .or. abs(TreeProcs(1)%PartType(1)) .eq. Chm_ )&
                & .and. ( abs(TreeProcs(1)%PartType(3)) .eq. Bot_ .or. abs(TreeProcs(1)%PartType(3)) .eq. Chm_) &
-               & .and. ( TreeProcs(1)%PartType(2) .eq. Z0_) ) then
+               & .and. ( TreeProcs(1)%PartType(2) .eq. Z0_ .or.  TreeProcs(1)%PartType(2) .eq. Pho_) ) then
              tag_Z=1
 !              print *, "setting tag_Z=1 (2-cut)"
           elseif ( (TreeProcs(2)%NumPart .eq. 3) .and. ( abs(TreeProcs(2)%PartType(1)) .eq. Bot_ .or. abs(TreeProcs(2)%PartType(1)) .eq. Chm_ )&
                & .and. ( abs(TreeProcs(2)%PartType(3)) .eq. Bot_ .or. abs(TreeProcs(2)%PartType(3)) .eq. Chm_) &
-               & .and. ( TreeProcs(2)%PartType(2) .eq. Z0_) ) then
+               & .and. ( TreeProcs(2)%PartType(2) .eq. Z0_ .or.  TreeProcs(2)%PartType(2) .eq. Pho_) ) then
              tag_Z = 1
 !              print *, "setting tag_Z=1 (2-cut) 2"
           else
              tag_Z=0
           endif
-
 
 !         set momentum vector for last particle
           if( TreeProcs(1)%PartType(TreeProcs(1)%NumPart).eq.Glu_ ) then
